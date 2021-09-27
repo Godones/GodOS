0e656d102dcd0b8d767880a9d1dbeb3c2a2d93c9
God <1925466036@qq.com>
2021-09-27 21:24:05 +0800

    remove target

--- bare_os/target/debug/deps/bare_os-75b05d043ee5ac32.d
+++
@@ -1,6 +0,0 @@
-/home/MyOS/GodOS/bare_os/target/debug/deps/bare_os-75b05d043ee5ac32: src/main.rs src/lang_items.rs
-
-/home/MyOS/GodOS/bare_os/target/debug/deps/bare_os-75b05d043ee5ac32.d: src/main.rs src/lang_items.rs
-
-src/main.rs:
-src/lang_items.rs:
