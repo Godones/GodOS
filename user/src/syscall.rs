996c76e1ae621d3748a52c7d8c504b8127bd5b17
God <1925466036@qq.com>
2021-11-26 22:16:41 +0800

    nothing

--- user/src/syscall.rs
+++ user/src/syscall.rs
@@ -20,19 +20,6 @@ pub fn sys_exit(state:i32) ->isize{
     syscall(SYSCALL_EXIT,[state as usize,0,0])//执行退出
 }
 
-
-// struct Stdout;
-// impl Write for Stdout{
-//     fn write_str(&mut self,s:&str)->fmt::Result{
-//         sys_write(1,s.as_bytes());
-//         Ok(())
-//     }
-// }
-//
-// pub fn print(args:fmt::Arguments){
-//     Stdout.write_fmt(args).unwrap();
-// }
-
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
     unsafe {
